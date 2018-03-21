3f430fee4672bf5082a04e08f52d8b47a43b4c0a
Michael <loukerismichalis@gmail.com>
2018-03-21 19:13:01 +0100

    make connection with api

--- app/src/main/java/com/sdu/andrumbrella/MainActivity.java
+++ app/src/main/java/com/sdu/andrumbrella/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sdu.andrumbrella;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,12 +9,16 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private TextView mErrorMessage;
     private ProgressBar mProgressBar;
     private WeatherAdapter mAdapter;
+    public static TextView mTestNet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView =  findViewById(R.id.weather_recyclerview);
         mErrorMessage =  findViewById(R.id.error_message);
         mProgressBar =  findViewById(R.id.loading_bar);
+        mTestNet = findViewById(R.id.test_net);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         mRecyclerView.setLayoutManager(layoutManager);
@@ -31,9 +37,35 @@ public class MainActivity extends AppCompatActivity {
         loadWeather();
     }
 
-
     public void loadWeather(){
-        mRecyclerView.setVisibility(View.VISIBLE);
+        mTestNet.setVisibility(View.VISIBLE);
+        mRecyclerView.setVisibility(View.INVISIBLE);
         mErrorMessage.setVisibility(View.INVISIBLE);
+        URL weatherUrl = NetworkCon.buildUrl("Patras");
+        new WeatherTask().execute(weatherUrl);
     }
+
+    public class WeatherTask extends AsyncTask<URL, Void, String> {
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL weatherUrl = urls[0];
+            String weatherResults = null;
+            try{
+                weatherResults = NetworkCon.getResponseFromHttpUrl(weatherUrl);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+            return weatherResults;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if(s != null && !s.equals("")){
+                mTestNet.setText(s);
+            }
+        }
+    }
+
+
 }
