05e88f2f7fbe63147bc561a41456f8cd1372390e
Michael <loukerismichalis@gmail.com>
2018-04-18 08:25:39 +0200

    final 1

--- app/build/generated/source/r/debug/android/support/compat/R.java
+++ app/build/generated/source/r/debug/android/support/compat/R.java
@@ -79,17 +79,17 @@ public final class R {
         public static final int italic = 0x7f070048;
         public static final int line1 = 0x7f07004b;
         public static final int line3 = 0x7f07004c;
-        public static final int normal = 0x7f070056;
-        public static final int notification_background = 0x7f070057;
-        public static final int notification_main_column = 0x7f070058;
-        public static final int notification_main_column_container = 0x7f070059;
-        public static final int right_icon = 0x7f070061;
-        public static final int right_side = 0x7f070062;
-        public static final int tag_transition_group = 0x7f070083;
-        public static final int text = 0x7f070084;
-        public static final int text2 = 0x7f070085;
-        public static final int time = 0x7f070088;
-        public static final int title = 0x7f070089;
+        public static final int normal = 0x7f070057;
+        public static final int notification_background = 0x7f070058;
+        public static final int notification_main_column = 0x7f070059;
+        public static final int notification_main_column_container = 0x7f07005a;
+        public static final int right_icon = 0x7f070062;
+        public static final int right_side = 0x7f070063;
+        public static final int tag_transition_group = 0x7f070084;
+        public static final int text = 0x7f070085;
+        public static final int text2 = 0x7f070086;
+        public static final int time = 0x7f070089;
+        public static final int title = 0x7f07008a;
     }
     public static final class integer {
         public static final int status_bar_notification_info_maxnum = 0x7f080004;
--- app/src/main/java/com/sdu/andrumbrella/MainActivity.java
+++ app/src/main/java/com/sdu/andrumbrella/MainActivity.java
@@ -1,6 +1,15 @@
 package com.sdu.andrumbrella;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +17,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
     private EditText cityName;
     private Switch metricSwitch;
     private Button searchButton;
+    private Geocoder geocoder;
+    private Button locationButton;
+    private LocationManager locationManager;
+    private static final int MY_PERMISSION_REQUEST_LOCATION = 1;
 
 
     @Override
@@ -27,8 +44,7 @@ public class MainActivity extends AppCompatActivity {
         metricSwitch = findViewById(R.id.metric);
         searchButton = findViewById(R.id.search);
         errorMessage = findViewById(R.id.errorMessage);
-
-
+        locationButton = findViewById(R.id.location);
 
         countryCode.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
@@ -59,24 +75,96 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 hideErrorMessage();
-                if(!countryCode.getText().toString().equals("") && !cityName.getText().toString().equals("")) {
+                if (!countryCode.getText().toString().equals("") && !cityName.getText().toString().equals("")) {
                     Intent startSearch = new Intent(MainActivity.this, UpcomingDays.class);
                     startSearch.putExtra("COUNTRY_CODE", countryCode.getText().toString());
                     startSearch.putExtra("CITY_NAME", cityName.getText().toString());
                     startSearch.putExtra("SWITCH_STATE", metricSwitch.isChecked());
                     startActivity(startSearch);
-                }else{
+                } else {
                     showErrorMessage();
                 }
             }
         });
+
+        locationButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick (View view){
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale
+                    (MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]
+                        {Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSION_REQUEST_LOCATION);
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]
+                        {Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSION_REQUEST_LOCATION);
+            }
+        } else {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            try {
+                String[] data = currentLocation(location.getLatitude(), location.getLongitude());
+                cityName.setText(data[0]);
+                countryCode.setText(data[1]);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(MainActivity.this, "Location not Found, make sure you have your GPS turned On", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    });
+}
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_REQUEST_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(MainActivity.this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        try {
+                            String[] data = currentLocation(location.getLatitude(), location.getLongitude());
+                            cityName.setText(data[0]);
+                            countryCode.setText(data[1]);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            Toast.makeText(MainActivity.this, "Location not Found, make sure you have your GPS turned On", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "No Permission Granted", Toast.LENGTH_SHORT).show();
+                }
+            }
+            return;
+        }
     }
 
-    public void showErrorMessage(){
+    public String[] currentLocation(double lat, double lon) {
+        String[] loc = new String[2];
+
+        geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+        List<Address> addressList;
+        try {
+            addressList = geocoder.getFromLocation(lat, lon, 1);
+            if (addressList.size() > 0) {
+                loc[0] = addressList.get(0).getLocality();
+                loc[1] = addressList.get(0).getCountryCode();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return loc;
+    }
+
+
+    public void showErrorMessage() {
         errorMessage.setVisibility(View.VISIBLE);
     }
 
-    public void hideErrorMessage(){
+    public void hideErrorMessage() {
         errorMessage.setVisibility(View.INVISIBLE);
     }
 }
